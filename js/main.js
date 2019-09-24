e3da00a02e669755b530d51171882000d8f1c373
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-09-24 13:39:34 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.height = scrolled + "%";
+} 
+
+
+function scrollToSection(id) {
+  var element = document.getElementsByName(id)[0];
+  element.scrollIntoView({behavior: "smooth"});
+}
