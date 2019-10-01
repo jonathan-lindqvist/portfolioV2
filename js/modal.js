b2935b7195f5dd1ddd124c3b0d8855dbdec72f4d
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-10-01 21:23:39 +0200

    Add 'budget' modal/lightbox for rails-react and gghub

--- js/modal.js
+++ js/modal.js
@@ -1,33 +1,33 @@
-
+//GGHub
 
 // Open the Modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+function openModalGG() {
+  document.getElementById("myModalGG").style.display = "block";
 }
 
 // Close the Modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+function closeModalGG() {
+  document.getElementById("myModalGG").style.display = "none";
 }
 
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlidesGG(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlidesGG(n) {
+  showSlidesGG(slideIndex += n);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlideGG(n) {
+  showSlidesGG(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlidesGG(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  var slides = document.getElementsByClassName("mySlidesGG");
+  var dots = document.getElementsByClassName("demoGG");
+  var captionText = document.getElementById("captionGG");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -39,4 +39,48 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+//Rails react
+
+// Open the Modal
+function openModalRR() {
+  document.getElementById("myModalRR").style.display = "block";
+}
+
+// Close the Modal
+function closeModalRR() {
+  document.getElementById("myModalRR").style.display = "none";
+}
+
+var slideIndexRR = 1;
+showSlidesRR(slideIndexRR);
+
+// Next/previous controls
+function plusSlidesRR(n) {
+  showSlidesRR(slideIndexRR += n);
+}
+
+// Thumbnail image controls
+function currentSlideRR(n) {
+  showSlidesRR(slideIndexRR = n);
+}
+
+function showSlidesRR(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlidesRR");
+  var dots = document.getElementsByClassName("demoRR");
+  var captionText = document.getElementById("captionRR");
+  if (n > slides.length) {slideIndexRR = 1}
+  if (n < 1) {slideIndexRR = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexRR-1].style.display = "block";
+  dots[slideIndexRR-1].className += " active";
+  captionText.innerHTML = dots[slideIndexRR-1].alt;
 }
