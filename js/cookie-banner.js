42a71fe2899561f0bdfcc2819b5280c833f352ce
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2026-04-05 12:08:57 +0200

    Change analytics to show visitors

--- js/cookie-banner.js
+++ js/cookie-banner.js
@@ -4,6 +4,8 @@ class CookieBanner {
     this.CONSENT_KEY = "analytics-consent";
     this.BANNER_ID = "cookie-banner";
     this.GA_MEASUREMENT_ID = "G-XG4NEZHBJM";
+    this.gaInitialized = false;
+    this.gaConfigured = false;
   }
 
   hasConsent() {
@@ -14,7 +16,9 @@ class CookieBanner {
     localStorage.setItem(this.CONSENT_KEY, value);
   }
 
-  loadGoogleAnalytics() {
+  initGoogleAnalytics() {
+    if (this.gaInitialized) return;
+
     const script = document.createElement("script");
     script.async = true;
     script.src = `https://www.googletagmanager.com/gtag/js?id=${this.GA_MEASUREMENT_ID}`;
@@ -27,9 +31,46 @@ class CookieBanner {
     document.head.appendChild(script);
 
     window.dataLayer = window.dataLayer || [];
-    const gtag = (...args) => window.dataLayer.push(args);
-    gtag("js", new Date());
-    gtag("config", this.GA_MEASUREMENT_ID);
+    window.gtag =
+      window.gtag ||
+      function gtag(...args) {
+        window.dataLayer.push(args);
+      };
+
+    window.gtag("js", new Date());
+    this.gaInitialized = true;
+  }
+
+  applyGoogleConsent(hasAccepted, mode = "default") {
+    if (typeof window.gtag !== "function") {
+      console.error("gtag is not initialized");
+      return;
+    }
+
+    // Denied mode allows basic cookieless measurement while blocking richer analytics features.
+    window.gtag("consent", mode, {
+      analytics_storage: hasAccepted ? "granted" : "denied",
+      ad_storage: "denied",
+      ad_user_data: "denied",
+      ad_personalization: "denied",
+    });
+  }
+
+  configureGoogleAnalytics() {
+    if (typeof window.gtag !== "function") {
+      console.error("gtag is not initialized");
+      return;
+    }
+    if (this.gaConfigured) return;
+
+    window.gtag("config", this.GA_MEASUREMENT_ID, {
+      anonymize_ip: true,
+      allow_google_signals: false,
+      allow_ad_personalization_signals: false,
+      send_page_view: true,
+    });
+
+    this.gaConfigured = true;
   }
 
   removeBanner() {
@@ -65,7 +106,7 @@ class CookieBanner {
     if (acceptBtn) {
       acceptBtn.addEventListener("click", () => {
         this.setConsent("true");
-        this.loadGoogleAnalytics();
+        this.applyGoogleConsent(true, "update");
         this.removeBanner();
       });
     }
@@ -73,6 +114,7 @@ class CookieBanner {
     if (declineBtn) {
       declineBtn.addEventListener("click", () => {
         this.setConsent("false");
+        this.applyGoogleConsent(false, "update");
         this.removeBanner();
       });
     }
@@ -81,14 +123,19 @@ class CookieBanner {
   init() {
     const consent = localStorage.getItem(this.CONSENT_KEY);
 
+    this.initGoogleAnalytics();
+
+    if (consent === "true") {
+      this.applyGoogleConsent(true, "default");
+    } else {
+      this.applyGoogleConsent(false, "default");
+    }
+    this.configureGoogleAnalytics();
+
     if (consent === null) {
       // No consent yet, show banner
       this.createBanner();
-    } else if (consent === "true") {
-      // User has consented, load GA
-      this.loadGoogleAnalytics();
     }
-    // If consent is 'false', do nothing
   }
 }
 
